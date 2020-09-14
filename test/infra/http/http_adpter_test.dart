dccee1d6c627a083ee730616dbb9024a5de774a0
Deivid Willyan <deiviid_@hotmail.com>
2020-09-14 10:00:47 -0300

    ensure HttpAdpter call post without body

--- test/infra/http/http_adpter_test.dart
+++ test/infra/http/http_adpter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
@@ -11,9 +13,11 @@ class HttpAdpter {
   Future<void> request({
     @required String url,
     @required String method,
+    Map body,
   }) async {
     final headers = {'content-type': 'application/json', 'accept': 'application/json'};
-    await client.post(url, headers: headers);
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    await client.post(url, headers: headers, body: jsonBody);
   }
 }
 
@@ -32,12 +36,19 @@ void main() {
 
   group('post', () {
     test('Should call post with correct values', () async {
-      await sut.request(url: url, method: 'post');
+      await sut.request(url: url, method: 'post', body: {"any_key": "any_value"});
 
       verify(client.post(
         url,
         headers: {'content-type': 'application/json', 'accept': 'application/json'},
+        body: '{"any_key":"any_value"}',
       ));
+    });
+
+    test('Should call post without body', () async {
+      await sut.request(url: url, method: 'post');
+
+      verify(client.post(any, headers: anyNamed('headers')));
     });
   });
 }
