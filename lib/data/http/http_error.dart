2d2e1c3877f03423f6b3c8744264c49cce304e72
Deivid Willyan <deiviid_@hotmail.com>
2020-09-14 08:12:51 -0300

    ensure RemoteAuthentication throws UnexpectedError with invalid data

--- lib/data/http/http_error.dart
+++ lib/data/http/http_error.dart
@@ -3,4 +3,5 @@ enum HttpError {
   notFound,
   serverError,
   unauthorized,
+  invalidData,
 }
--- lib/data/models/remote_account_model.dart
+++ lib/data/models/remote_account_model.dart
@@ -1,11 +1,18 @@
-import '../../domain/entities/account_entity.dart';
+import '../../domain/entities/entities.dart';
+
+import '../http/http.dart';
 
 class RemoteAccountModel {
   final String accessToken;
 
   RemoteAccountModel(this.accessToken);
 
-  factory RemoteAccountModel.fromMap(Map map) => RemoteAccountModel(map['accessToken']);
+  factory RemoteAccountModel.fromMap(Map map) {
+    if (!map.containsKey('accessToken')) {
+      throw HttpError.invalidData;
+    }
+    return RemoteAccountModel(map['accessToken']);
+  }
 
   AccountEntity toEntity() => AccountEntity(accessToken);
 }
