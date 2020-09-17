ad23832584a94ed12025048bd62cffdcc3d73607
Deivid Willyan <deiviid_@hotmail.com>
2020-09-17 08:38:40 -0300

    ensure form is valid

--- lib/presentation/presenters/stream_login_presenter.dart
+++ lib/presentation/presenters/stream_login_presenter.dart
@@ -4,9 +4,11 @@ import 'package:meta/meta.dart';
 import '../protocols/protocols.dart';
 
 class LoginState {
+  String email;
   String emailError;
+  String password;
   String passwordError;
-  bool get isFormValid => false;
+  bool get isFormValid => emailError == null && passwordError == null && email != null && password != null;
 }
 
 class StreamLoginPresenter {
@@ -20,13 +22,17 @@ class StreamLoginPresenter {
   Stream get passwordErrorStream => _streamController.stream.map((state) => state.passwordError).distinct();
   Stream get isFormValidStream => _streamController.stream.map((state) => state.isFormValid).distinct();
 
+  void _update() => _streamController.add(_state);
+
   void validateEmail(String email) {
+    _state.email = email;
     _state.emailError = validation.validate(field: 'email', value: email);
-    _streamController.add(_state);
+    _update();
   }
 
   void validatePassword(String password) {
+    _state.password = password;
     _state.passwordError = validation.validate(field: 'password', value: password);
-    _streamController.add(_state);
+    _update();
   }
 }
--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -85,4 +85,14 @@ void main() {
     sut.validateEmail(email);
     sut.validatePassword(password);
   });
+
+  test('Should form is valid', () async {
+    sut.emailErrorStream.listen(expectAsync1((error) => expect(error, null)));
+    sut.passwordErrorStream.listen(expectAsync1((error) => expect(error, null)));
+    expectLater(sut.isFormValidStream, emitsInOrder([false, true]));
+
+    sut.validateEmail(email);
+    await Future.delayed(Duration.zero);
+    sut.validatePassword(password);
+  });
 }
