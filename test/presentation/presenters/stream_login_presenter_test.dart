27261e77754db225afe4645dd5a41cea4f5c8c7b
Deivid Willyan <deiviid_@hotmail.com>
2020-09-17 08:21:01 -0300

    ensure emit password error if validations fails

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -56,4 +56,14 @@ void main() {
 
     verify(validation.validate(field: 'password', value: password)).called(1);
   });
+
+  test('Should emit password error if validations fails', () {
+    mockValidation(value: 'error');
+
+    sut.passwordErrorStream.listen(expectAsync1((error) => expect(error, 'error')));
+    sut.isFormValidStream.listen(expectAsync1((isValid) => expect(isValid, false)));
+
+    sut.validatePassword(email);
+    sut.validatePassword(email);
+  });
 }
