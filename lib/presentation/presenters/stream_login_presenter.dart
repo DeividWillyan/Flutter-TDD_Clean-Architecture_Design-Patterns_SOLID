4f84ac8c1d0ab6d0a525c2a53832f25902fd0f59
Deivid Willyan <deiviid_@hotmail.com>
2020-09-17 08:19:04 -0300

    ensure calls validation when alter password

--- lib/presentation/presenters/stream_login_presenter.dart
+++ lib/presentation/presenters/stream_login_presenter.dart
@@ -5,6 +5,7 @@ import '../protocols/protocols.dart';
 
 class LoginState {
   String emailError;
+  String passwordError;
   bool get isFormValid => false;
 }
 
@@ -16,10 +17,16 @@ class StreamLoginPresenter {
   var _state = LoginState();
 
   Stream get emailErrorStream => _streamController.stream.map((state) => state.emailError).distinct();
+  Stream get passwordErrorStream => _streamController.stream.map((state) => state.passwordError).distinct();
   Stream get isFormValidStream => _streamController.stream.map((state) => state.isFormValid).distinct();
 
   void validateEmail(String email) {
     _state.emailError = validation.validate(field: 'email', value: email);
+    _streamController.add(_state);
+  }
+
+  void validatePassword(String password) {
+    _state.passwordError = validation.validate(field: 'password', value: password);
     _streamController.add(_state);
   }
 }
--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -11,6 +11,7 @@ void main() {
   ValidationSpy validation;
   StreamLoginPresenter sut;
   String email;
+  String password;
 
   PostExpectation mockValidationCall({String field}) =>
       when(validation.validate(field: field ?? anyNamed('field'), value: anyNamed('value')));
@@ -21,6 +22,7 @@ void main() {
     validation = ValidationSpy();
     sut = StreamLoginPresenter(validation: validation);
     email = faker.internet.email();
+    password = faker.internet.password();
 
     mockValidation();
   });
@@ -47,5 +49,11 @@ void main() {
 
     sut.validateEmail(email);
     sut.validateEmail(email);
+  });
+
+  test('Should call Validation with correct password', () {
+    sut.validatePassword(password);
+
+    verify(validation.validate(field: 'password', value: password)).called(1);
   });
 }
