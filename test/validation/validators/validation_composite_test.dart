781bef2026d49e40cfe5dc8e402dc9ad920d5688
Deivid Willyan <deiviid_@hotmail.com>
2020-09-18 08:24:11 -0300

    refactor validation composite test

--- test/validation/validators/validation_composite_test.dart
+++ test/validation/validators/validation_composite_test.dart
@@ -15,17 +15,26 @@ class ValidationComposite implements Validation {
 class FieldValidationSpy extends Mock implements FieldValidation {}
 
 void main() {
+  ValidationComposite sut;
+  FieldValidationSpy validation1;
+  FieldValidationSpy validation2;
+
+  void mockValidation(FieldValidationSpy validation, String error) {
+    when(validation.field).thenReturn('any_field');
+    when(validation.validate(any)).thenReturn(error);
+  }
+
+  setUp(() {
+    validation1 = FieldValidationSpy();
+    mockValidation(validation1, null);
+
+    validation2 = FieldValidationSpy();
+    mockValidation(validation2, '');
+
+    sut = ValidationComposite([validation1, validation2]);
+  });
+
   test('Shold return null if all validations returns null or empty', () {
-    final validation1 = FieldValidationSpy();
-    when(validation1.field).thenReturn('any_field');
-    when(validation1.validate(any)).thenReturn(null);
-
-    final validation2 = FieldValidationSpy();
-    when(validation2.field).thenReturn('any_field');
-    when(validation2.validate(any)).thenReturn('');
-
-    final sut = ValidationComposite([validation1, validation2]);
-
     final error = sut.validate(field: 'any_field', value: 'any_value');
 
     expect(error, null);
