88872165c47603dacb4664b8f54aea1516aaa04f
Deivid Willyan <deiviid_@hotmail.com>
2020-09-22 07:40:00 -0300

    ensure SaveSecureCacheStorage return UnexpectedError if throw

--- test/data/usecases/save_current_account/local_save_current_account_test.dart
+++ test/data/usecases/save_current_account/local_save_current_account_test.dart
@@ -1,5 +1,6 @@
 import 'package:faker/faker.dart';
 import 'package:flutter_avancado/domain/entities/entities.dart';
+import 'package:flutter_avancado/domain/helpers/helpers.dart';
 import 'package:flutter_avancado/domain/usecases/usecases.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
@@ -11,7 +12,11 @@ class LocalSaveCurrentAccout implements SaveCurrentAccount {
 
   @override
   Future<void> save(AccountEntity account) async {
-    saveSecureCacheStorage.saveSecure(key: 'token', value: account.token);
+    try {
+      saveSecureCacheStorage.saveSecure(key: 'token', value: account.token);
+    } catch (error) {
+      throw DomainError.unexpected;
+    }
   }
 }
 
@@ -22,7 +27,7 @@ abstract class SaveSecureCacheStorage {
 class SaveSecureCacheStorageSpy extends Mock implements SaveSecureCacheStorage {}
 
 void main() {
-  test('Should call SaveCacheStorage with correct values', () async {
+  test('Should call SaveSecureCacheStorage with correct values', () async {
     final saveSecureCacheStorage = SaveSecureCacheStorageSpy();
     final sut = LocalSaveCurrentAccout(saveSecureCacheStorage: saveSecureCacheStorage);
     final account = AccountEntity(faker.guid.guid());
@@ -30,5 +35,17 @@ void main() {
     await sut.save(account);
 
     verify(saveSecureCacheStorage.saveSecure(key: 'token', value: account.token));
+  });
+
+  test('Should throw UnexpectedError if SaveSecureCacheStorage throws', () async {
+    final saveSecureCacheStorage = SaveSecureCacheStorageSpy();
+    final sut = LocalSaveCurrentAccout(saveSecureCacheStorage: saveSecureCacheStorage);
+    final account = AccountEntity(faker.guid.guid());
+    when(saveSecureCacheStorage.saveSecure(key: anyNamed('key'), value: anyNamed('value')))
+        .thenThrow(Exception());
+
+    final result = sut.save(account);
+
+    expect(result, throwsA(DomainError.unexpected));
   });
 }
