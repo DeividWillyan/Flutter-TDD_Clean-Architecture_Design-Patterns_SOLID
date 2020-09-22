a7ad98544a161d302cab29c28742197e1148fc4f
Deivid Willyan <deiviid_@hotmail.com>
2020-09-22 08:01:57 -0300

    ensure local storage adpter save secure with correct values

---
+++ test/infra/cache/local_storage_adpter_test.dart
@@ -0,0 +1,32 @@
+import 'package:faker/faker.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+import 'package:meta/meta.dart';
+
+import 'package:flutter_avancado/data/cache/cache.dart';
+
+class LocalStorageAdpter implements SaveSecureCacheStorage {
+  final FlutterSecureStorage secureStorage;
+  LocalStorageAdpter({@required this.secureStorage});
+
+  @override
+  Future<void> saveSecure({@required String key, @required String value}) async {
+    await secureStorage.write(key: key, value: value);
+  }
+}
+
+class FlutterSecureStorageSpy extends Mock implements FlutterSecureStorage {}
+
+main() {
+  test('Should call save secure with correct values', () async {
+    final secureStorage = FlutterSecureStorageSpy();
+    final sut = LocalStorageAdpter(secureStorage: secureStorage);
+    final key = faker.lorem.word();
+    final value = faker.guid.guid();
+
+    await sut.saveSecure(key: key, value: value);
+
+    verify(secureStorage.write(key: key, value: value));
+  });
+}
