c6e8f44167bc1bda09ae8a706b55d365c119baec
Deivid Willyan <deiviid_@hotmail.com>
2020-09-23 08:10:02 -0300

    refactor adpter to implements correct interface

--- lib/infra/cache/local_storage_adpter.dart
+++ lib/infra/cache/local_storage_adpter.dart
@@ -3,14 +3,14 @@ import 'package:meta/meta.dart';
 
 import '../../data/cache/cache.dart';
 
-class LocalStorageAdpter implements SaveSecureCacheStorage {
+class LocalStorageAdpter implements SaveSecureCacheStorage, FetchSecureCacheStorage {
   final FlutterSecureStorage secureStorage;
   LocalStorageAdpter({@required this.secureStorage});
 
   @override
-  Future<void> saveSecure({@required String key, @required String value}) async {
-    await secureStorage.write(key: key, value: value);
-  }
+  Future<void> saveSecure({@required String key, @required String value}) async =>
+      await secureStorage.write(key: key, value: value);
 
+  @override
   Future<String> fetchSecure(String key) async => await secureStorage.read(key: key);
 }
