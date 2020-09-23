3eb527378650b68b9f808907d39b6aab503b0fee
Deivid Willyan <deiviid_@hotmail.com>
2020-09-23 15:54:31 -0300

    ensure splash call loadCurrentAccount

--- test/ui/page/splash_page_test.dart
+++ test/ui/page/splash_page_test.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
+import 'package:mockito/mockito.dart';
 
 class SplashPage extends StatelessWidget {
+  final SplashPresenter presenter;
+  SplashPage({this.presenter});
+
   @override
   Widget build(BuildContext context) {
+    presenter.loadCurrentAccount();
     return Container(
       color: Colors.white,
       height: MediaQuery.of(context).size.height,
@@ -16,11 +21,23 @@ class SplashPage extends StatelessWidget {
   }
 }
 
+abstract class SplashPresenter {
+  Future<void> loadCurrentAccount();
+}
+
+class SplashPresenterSpy extends Mock implements SplashPresenter {}
+
 void main() {
+  SplashPresenterSpy presenter;
+
+  setUp(() {
+    presenter = SplashPresenterSpy();
+  });
+
   loadPage(tester) async => await tester.pumpWidget(
         GetMaterialApp(
           initialRoute: '/',
-          getPages: [GetPage(name: '/', page: () => SplashPage())],
+          getPages: [GetPage(name: '/', page: () => SplashPage(presenter: presenter))],
         ),
       );
 
@@ -28,5 +45,11 @@ void main() {
     await loadPage(tester);
 
     expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+
+  testWidgets("Should call loadCurrentAccount in presenter", (WidgetTester tester) async {
+    await loadPage(tester);
+
+    verify(presenter.loadCurrentAccount()).called(1);
   });
 }
