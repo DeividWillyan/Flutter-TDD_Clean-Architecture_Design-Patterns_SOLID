7ea789214c654bd096e7e03190095aed821ca47d
Deivid Willyan <deiviid_@hotmail.com>
2020-09-23 07:53:39 -0300

    move to production files

--- test/data/usecases/load_current_account/load_current_account_test.dart
+++ test/data/usecases/load_current_account/load_current_account_test.dart
@@ -1,28 +1,10 @@
 import 'package:faker/faker.dart';
+import 'package:flutter_avancado/data/cache/cache.dart';
+import 'package:flutter_avancado/data/usecases/usecases.dart';
 import 'package:flutter_avancado/domain/entities/account_entity.dart';
 import 'package:flutter_avancado/domain/helpers/helpers.dart';
-import 'package:flutter_avancado/domain/usecases/load_current_account.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
-import 'package:meta/meta.dart';
-
-class LocalLoadCurrentAccount implements LoadCurrentAccount {
-  final FetchSecureCacheStorage fetchSecureCacheStorage;
-  LocalLoadCurrentAccount({@required this.fetchSecureCacheStorage});
-
-  Future<AccountEntity> load() async {
-    try {
-      final token = await fetchSecureCacheStorage.fetchSecure('token');
-      return AccountEntity(token);
-    } catch (error) {
-      throw DomainError.unexpected;
-    }
-  }
-}
-
-abstract class FetchSecureCacheStorage {
-  Future<String> fetchSecure(String key);
-}
 
 class FetchSecureCacheStorageSpy extends Mock implements FetchSecureCacheStorage {}
 
